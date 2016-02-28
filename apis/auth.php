838965ec2efa3018463d8174bfcd6f0c1c859b32
Danilo "DMZK" Metzker <danilometzker@gmail.com>
2016-02-28 13:37:45 -0400

    Update auth.php

--- apis/auth.php
+++ apis/auth.php
@@ -1,11 +1,40 @@
+<?php if(isset($_GET['g_inutil_login']): ?>
 <DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <title>Aguarde...</title>
     <script src="https://apis.google.com/js/platform.js" async defer></script>
-	<meta name="google-signin-client_id" content="116454895004-e255eu26vcosjdfk5vqm8a6vtbcgr56p.apps.googleusercontent.com">
+    <meta name="google-signin-client_id" content="116454895004-e255eu26vcosjdfk5vqm8a6vtbcgr56p.apps.googleusercontent.com">
 </head>
-<body>
+<?php if(isset($_GET['g_signin']): ?>
+<body onload="signIn()">
+<?php elseif(isset($_GET['g_signout']): ?>
+<body onload="signOut()">
+<?php endif; ?>
+	<script>
+	function signIn(googleUser) {
+		
+		var profile = googleUser.getBasicProfile();
+		console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+		console.log("Name: " + profile.getName());
+		console.log("Image URL: " + profile.getImageUrl());
+		console.log("Email: " + profile.getEmail());
+						
+		// The ID token you need to pass to your backend:
+		var id_token = googleUser.getAuthResponse().id_token;
+		 console.log("ID Token: " + id_token);
+	};
+	</script>
+						
+	<script>
+	function signOut() {
+		var auth2 = gapi.auth2.getAuthInstance();
+		auth2.signOut().then(function () {
+			console.log('User signed out.');
+		});
+	}
+	</script>
 </body>
 </html>
+<?php endif; ?>
