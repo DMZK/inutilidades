96fd19f41aaa31c555aa014283d6f6d0cf4a3674
Danilo "DMZK" Metzker <danilometzker@gmail.com>
2016-02-28 13:32:11 -0400

    Create auth.php

---
+++ apis/auth.php
@@ -0,0 +1,11 @@
+<DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Aguarde...</title>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+	<meta name="google-signin-client_id" content="116454895004-e255eu26vcosjdfk5vqm8a6vtbcgr56p.apps.googleusercontent.com">
+</head>
+<body>
+</body>
+</html>
